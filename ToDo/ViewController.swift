82f4e39969f0abef790298cb8c4fc114955ad74c
luiz <lbrebelatto@gmail.com>
2023-05-01 14:07:46 -0300

    feat: create table to insert our tasks

--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -7,13 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    private let table: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "To do List"
+        view.addSubview(table)
+        table.dataSource = self
     }
-
-
+    
 }
 
